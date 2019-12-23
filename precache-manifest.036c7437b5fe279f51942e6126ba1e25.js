94e5b1eb3c12fbb80c54243c4d8b57913a573512
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2019-12-23 18:36:57 +0200

    Updates

---
+++ precache-manifest.036c7437b5fe279f51942e6126ba1e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81352c8fb55263c80e53a96b01755c65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba5dbd32d4a6dfd50527",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f174afb40475fb356f48",
+    "url": "/static/js/2.f86787de.chunk.js"
+  },
+  {
+    "revision": "8d56319702959215daf3bbc97856f05a",
+    "url": "/static/js/2.f86787de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba5dbd32d4a6dfd50527",
+    "url": "/static/js/main.ac822876.chunk.js"
+  },
+  {
+    "revision": "aa340c3b391355d322fd",
+    "url": "/static/js/runtime-main.c1cfcce9.js"
+  }
+]);
