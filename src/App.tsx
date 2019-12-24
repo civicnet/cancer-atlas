20cb48b44ed9bc306934701c8c2bab40aa1cc547
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2019-12-24 10:17:29 +0200

    fix(legend) titles on hover

--- src/App.tsx
+++ src/App.tsx
@@ -420,12 +420,14 @@ const App: React.FC = () => {
                 >
                   {idx === 0 && (
                     <Icon
+                      title="Zone cu număr mai mic de furnizori de servicii medicale"
                       className="fal fa-long-arrow-alt-down"
                       style={{ color: "#fff", alignSelf: "center" }}
                     />
                   )}
                   {idx === 5 && (
                     <Icon
+                    title="Zone cu număr mai mare de furnizori de servicii medicale"
                       className="fal fa-long-arrow-alt-up"
                       style={{ color: "#333", alignSelf: "center" }}
                     />
